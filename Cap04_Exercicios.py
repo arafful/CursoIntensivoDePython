7530d6ad13aa2a8c41ace22f4525e0aece06c1b8
ANDRE RAFFUL <andre.rafful@gmail.com>
2022-03-17 15:59:49 -0300

    CAPITULO 4

---
+++ Cap04_Exercicios.py
@@ -0,0 +1,19 @@
+"""
+    Exercícios Capítulo 4
+"""
+# 4.1 - Pizzas
+pizzas = ["peperoni", "marguerita", "calabreza"]
+
+for pizza in pizzas:
+    print("Vamos pedir uma pizza grande de", pizza.title())
+
+print("\nPizza tem que ser grande e com borda recheada.\n\n")
+
+# 4.2 - Animais
+bichos = ["papagaio", "gato", "cachorro"]
+
+for bicho in bichos:
+    print("Um", bicho, "seria um ótimo animal de estimação.")
+
+print("\nEu gosto de ter pets.\n\n")
+
--- Cap04_Trabalhando_Listas.py
+++ Cap04_Trabalhando_Listas.py
@@ -1,3 +1,31 @@
 """
     Capítulo 4 - Trabalhando com Listas
 """
+magicos = ["alice", "david", "carolina"]
+
+for magico in magicos:
+    print((magico.title()), "parabéns, grande truque.")
+    print("Mal posso esperar pelo próximo truque!\n")
+
+print("Obrigado a todos vocês, foi um ótimo show de mágicas.")
+
+for i in range(1, 5):
+    print(i)
+print("\n")
+
+numeros = list(range(1, 6))
+for numero in numeros:
+    print(numero)
+print("\n")
+
+numeros_pares = list(range(2, 11, 2))
+for par in numeros_pares:
+    print(par)
+print("\n")
+
+quadrados = []
+for i in range(1, 11):
+    quadrados.append(i ** 2)
+
+print(quadrados)
+print("\n")
