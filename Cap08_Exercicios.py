63f491ff97593c8dc87253f7b98969da85d69af8
ANDRE RAFFUL <andre.rafful@gmail.com>
2022-03-25 16:21:45 -0300

    CAPITULO 8

--- Cap08_Exercicios.py
+++ Cap08_Exercicios.py
@@ -12,6 +12,7 @@ def display_message():
 
 display_message()
 
+# 8.2 - Livros
 print("\n===< EXERCÍCIO 8.2 >===")
 
 
@@ -20,3 +21,54 @@ def livro_favorito(nome_livro):
 
 
 livro_favorito("O Caso dos Dez Negrinhos")
+
+# 8.3 - Camisetas
+print("\n===< EXERCÍCIO 8.3 >===")
+
+
+def make_shirt(tamanho, texto):
+    print('\nO tamanho da camiseta será', tamanho + ',')
+    print('e a frase será:', texto + '.')
+
+
+make_shirt('GG', 'Salvem as baleias')
+make_shirt(tamanho='P', texto='O sentimento não pode parar')
+
+# 8.4 - Camisetas Grandes
+print("\n===< EXERCÍCIO 8.4 >===")
+
+
+def make_shirt2(tamanho='G', texto='Eu amo Python'):
+    print('\nO tamanho da camiseta será', tamanho + ',')
+    print('e a frase será:', texto + '.')
+
+
+make_shirt2()
+make_shirt2(tamanho='P')
+make_shirt2(tamanho='M')
+make_shirt2(texto='C++ já era')
+
+# 8.5 - Cidades
+print("\n===< EXERCÍCIO 8.5 >===")
+
+
+def describe_city(nome_cidade, pais_cidade='Brasil'):
+    print('\nCidade:', nome_cidade.title(), '- País:', pais_cidade.title() + ',')
+
+describe_city('rio de janeiro')
+describe_city('campinas')
+describe_city('New York', 'USA')
+describe_city(nome_cidade='méxico', pais_cidade='méxico')
+
+# 8.6 - Nomes de Cidades
+print("\n===< EXERCÍCIO 8.6 >===")
+
+
+def city_country(city_name, country_name):
+    print(city_name.title() + ' , ' + country_name.title())
+
+
+city_country('rio de janeiro', 'brasil')
+city_country('campinas', 'brasil')
+city_country('Buenos Aires', 'argentina')
+
--- Cap08_Funcoes.py
+++ Cap08_Funcoes.py
@@ -24,3 +24,43 @@ descreve_pet('dushround', 'xuxa')
 descreve_pet(nome_animal='kiara', tipo_animal='pit bull')
 
 descreve_pet(nome_animal='Kiara')
+
+
+def nome_formatado(nome, sobrenome):
+    # Retorna um nome completo, formatado de forma elegante.
+    nome_completo = nome + ' ' + sobrenome
+    return nome_completo.title()
+
+
+magico = nome_formatado('andré', 'rafful')
+print('\n' + magico)
+
+
+def build_person(first_name, last_name, age=''):
+    # Devolve um dicionário com informações sobre uma pessoa
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = build_person('jimi', 'hendrix')
+print(musician)
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician)
+
+
+def get_formatted_name(first_name, last_name):
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+while True:
+    print('\nPlease tell me your name:')
+    print("Enter 'q' at any time to exit.")
+    fname = input("First name: ")
+    if fname == 'q':
+        break
+    lname = input("Last name: ")
+    if lname == 'q':
+        break
+    formatted_name = get_formatted_name(fname, lname)
+    print("\nHello", formatted_name + '!')
