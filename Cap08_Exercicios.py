1d801393d5ff07608aa45790e576fe10c54d5d2c
ANDRE RAFFUL <andre.rafful@gmail.com>
2022-03-25 14:41:10 -0300

    CAPITULO 8

---
+++ Cap08_Exercicios.py
@@ -0,0 +1,22 @@
+"""
+    Capítulo 8 - Exercícios
+"""
+# 8.1 - Mensagens
+
+print("===< EXERCÍCIO 8.1 >===")
+
+
+def display_message():
+    print("Capitulo 8 - Funções")
+
+
+display_message()
+
+print("\n===< EXERCÍCIO 8.2 >===")
+
+
+def livro_favorito(nome_livro):
+    print('O meu livro favorito é: "' + nome_livro + '".')
+
+
+livro_favorito("O Caso dos Dez Negrinhos")
--- Cap08_Funcoes.py
+++ Cap08_Funcoes.py
@@ -1,3 +1,26 @@
 """
     Capítulo 8 - Funções
 """
+
+
+# Exibe uma saudação simples
+def saudacao_usuario(nome_usuario):
+    print("Olá", nome_usuario.title() + '.')
+
+
+saudacao_usuario('andre')
+saudacao_usuario('monica')
+
+
+def descreve_pet(tipo_animal='cachorro', nome_animal=''):
+    print("\nEu tenho um", tipo_animal + '.')
+    print('Meu', tipo_animal, 'se chama', nome_animal.title() + '.')
+
+
+descreve_pet('pit bull', 'kiara')
+descreve_pet('vira-lata', 'piná')
+descreve_pet('dushround', 'xuxa')
+
+descreve_pet(nome_animal='kiara', tipo_animal='pit bull')
+
+descreve_pet(nome_animal='Kiara')
