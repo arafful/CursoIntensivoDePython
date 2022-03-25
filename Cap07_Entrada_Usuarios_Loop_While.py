3450cbca396d8203c73c75520c1ecb86fd278ee3
ANDRE RAFFUL <andre.rafful@gmail.com>
2022-03-25 12:01:28 -0300

    CAPITULO 7

--- Cap07_Entrada_Usuarios_Loop_While.py
+++ Cap07_Entrada_Usuarios_Loop_While.py
@@ -28,3 +28,48 @@ current_number = 1
 while current_number <= 5:
     print(current_number)
     current_number += 1
+
+usuarios_confirmados = []
+usuarios_nao_confirmados = ['alice', 'brian', 'candance']
+
+# Confirma cada usuário, até que não haja mais usuários não confirmados
+
+while usuarios_nao_confirmados:
+    usuario_corrente = usuarios_nao_confirmados.pop()
+    print("Verificando usuário:", usuario_corrente.title())
+    usuarios_confirmados.append(usuario_corrente)
+
+# Exibe todos os usuários confirmados
+print("\nOs seguintes usuários foram confirmados:")
+for usuario_confirmado in usuarios_confirmados:
+    print(usuario_confirmado.title())
+
+print(usuarios_nao_confirmados)
+
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+
+print(pets)
+
+respostas = {}
+ativo = True
+
+while ativo:
+    # Pede o nome e a resposta da pesquisa
+    nome = input("\nQual o seu nome? ")
+    resposta = input("Para qual time você torce? ")
+    # Armazena a resposta da pesquisa em um dicionário
+    respostas[nome] = resposta
+    # Verifica se mais alguém vai responder a pesquisa
+    flag = input("Mais alguém vai responder a pesquisa? (Sim/Não")
+    if flag != 'Sim':
+        ativo = False
+
+# Mostra o resultado da pesquisa
+for nome, resposta in respostas.items():
+    print(nome, "torce pelo", resposta + '.')
+
+
