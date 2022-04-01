5e9567738d065180df2057e252e40f41fe9bf450
ANDRE RAFFUL <andre.rafful@gmail.com>
2022-04-01 14:27:07 -0300

    CAPITULO 10 + TEXTOS

--- Cap10_Arquivos_Excecoes.py
+++ Cap10_Arquivos_Excecoes.py
@@ -9,3 +9,94 @@ print('')
 with open('pi_digits.txt') as file_object:
     for line in file_object:
         print(line.rstrip())
+
+print('')
+with open('pi_digits.txt') as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+
+print(pi_string)
+print(len(pi_string))
+
+print('')
+with open('pi_million_digits.txt') as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+
+print(pi_string[:52] + '...')
+print(len(pi_string))
+
+aniversario = '17101964'
+if aniversario in pi_string:
+    print("Seu aniversário em 17/10/1964 está presente em pi.")
+else:
+    print("Seu aniversário em 17/10/1964 não está presente em pi.")
+
+filename = 'programming.txt'
+with open(filename, 'w') as file_object:
+    """ O parâmetro 'w' abre um arquivo vazio, mesmo que ele ja´existe o conteudo será apagado. """
+    file_object.write('Teste de gravação de arquivo no python.\n')
+    file_object.write('Nova linha de arquivo.\n')
+
+with open(filename, 'a') as file_object:
+    """ O parâmetro 'a' abre um arquivo para append, mesmo que ele não exista. """
+    file_object.write('Acrescentando linhas com o parâmetro "a" na cláusula open.\n')
+    file_object.write('Nova linha de arquivo.\n')
+
+try:
+    print(5 / 0)
+except ZeroDivisionError:
+    print("Não pode dividir por zero.")
+
+print("Digite dois numeros e eu vou dividí-los: ")
+
+while True:
+    numerador = input("\nNumerador: ")
+    if numerador == '':
+        break
+    denominador = input("Denominador: ")
+    if denominador == '':
+        break
+
+    try:
+        resposta = int(numerador) / int(denominador)
+    except ZeroDivisionError:
+        print("Divisão inválida.")
+    else:
+        print("Resposta: ", resposta)
+
+
+def conta_palavras(arquivo):
+    """
+    Conta o númer aproximado de palavras em um arquivo
+    :param arquivo: Nome do arquivo a ser lido
+    :return:
+    """
+    try:
+        with open(filename) as f_obj:
+            conteudo = f_obj.read()
+    except FileNotFoundError:
+        msg = "Desculpe, o arquivo " + filename + ' não existe.'
+        print(msg)
+    except UnicodeDecodeError:
+        msg = "Erro de decodificador de caractaer para o arquivo " + filename
+        print(msg)
+    else:
+        # conta o numero aproximado de palavras no texto #
+        palavras = conteudo.split()
+        num_palavras = len(palavras)
+        print("O arquivo " + filename + " tem aproximadamente " + str(num_palavras) + " palavras.")
+
+
+filename = 'aprendendo_python.txt'
+conta_palavras(filename)
+
+filenames = ['alice.txt', 'moby_dick.txt', 'little_women.txt', 'siddhartha.txt']
+for filename in filenames:
+    conta_palavras(filename)
--- Cap10_Exercicios.py
+++ Cap10_Exercicios.py
@@ -1,5 +1,102 @@
 """
     Capítulo 10 - Exercícios
 """
-# Exercício 10.1 -
+# Exercício 10.1 - Time do Vasco
 print("============< EXERCÍCIO 10.1 >=============")
+with open('aprendendo_python.txt') as arquivo_texto:
+    conteudo = arquivo_texto.read()
+    print(conteudo.rstrip())
+
+with open('aprendendo_python.txt') as arquivo_texto:
+    for linha in arquivo_texto:
+        print(linha.rstrip())
+
+with open('aprendendo_python.txt') as arquivo_texto:
+    linhas = arquivo_texto.readlines()
+
+for linha in linhas:
+    print(linha.rstrip())
+
+# Exercício 10.2 - Substituindo texto
+print("============< EXERCÍCIO 10.2 >=============")
+
+with open('aprendendo_python.txt') as arquivo_texto:
+    linhas = arquivo_texto.readlines()
+
+for linha in linhas:
+    print(linha.replace('Figueiredo', 'Ariel').rstrip())
+
+# Exercício 10.3 - Convidado
+print("============< EXERCÍCIO 10.3 >=============")
+
+convidado = input("Digite seu nome: ")
+with open('guest.txt', 'w') as arquivo_exerc_10_3:
+    arquivo_exerc_10_3.write(convidado)
+
+# Exercício 10.4 - Lista de Convidados
+print("============< EXERCÍCIO 10.4 >=============")
+
+with open('guest_book.txt', 'w') as arquivo_exerc_10_4:
+
+    while True:
+        convidado = input("Digite seu nome: ")
+        if convidado != '':
+            arquivo_exerc_10_4.write(convidado + '\n')
+        else:
+            break
+
+# Exercício 10.6 - Adição
+print("============< EXERCÍCIO 10.6 + 10.7 >=============")
+
+print("\nDigite duas parcelas numéricas para soma:")
+while True:
+    num_01 = input("Primeira parcela: ")
+    if num_01 == '':
+        break
+    num_02 = input("Segunda  parcela: ")
+    if num_02 == '':
+        break
+    try:
+        resultado = int(num_01) + int(num_02)
+    except ValueError:
+        print('Digite valores numéricos ou "" para sair.')
+    else:
+        print("Resultado = " + str(resultado))
+
+# Exercício 10.8 - Gatos e cachorros
+print("============< EXERCÍCIO 10.8 + 10.9 >=============")
+arquivos = ['gatos.txt', 'cachorros.txt', 'passaros.txt']
+
+for arquivo in arquivos:
+    try:
+        with open(arquivo) as arquivo_aberto:
+            linhas = arquivo_aberto.readlines()
+            print("Nomes no arquivo " + arquivo)
+            for linha in linhas:
+                print("\t" + linha.rstrip())
+    except FileNotFoundError:
+        pass
+        #print("================================")
+        #print("Não existe o arquivo " + arquivo)
+        #print("================================")
+
+
+# Exercício 10.10 - Palavras comuns
+print("============< EXERCÍCIO 10.10 >=============")
+
+arquivo = 'WildernessHoney.txt'
+
+try:
+    with open(arquivo) as obj_arq:
+        conteudo = obj_arq.read()
+        contador = conteudo.lower().count('the')
+        print("A palavra 'The' aparece " + str(contador) + " vezes.")
+except FileNotFoundError:
+    pass
+
+
+
+
+
+
+
